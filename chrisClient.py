783fa98a23b9c1e24fbde3fa5af99c7bad74a7bb
Sandip Samal <sandip.samal@childrens.harvard.edu>
2024-12-17 15:58:20 -0500

    code clean up

--- chrisClient.py
+++ chrisClient.py
@@ -68,34 +68,6 @@ class ChrisClient(BaseClient):
         pv_in_id = self.__create_feed(pl_id, {"previous_id": pv_id, 'dir': dicom_dir})
         return int(pv_in_id)
 
-    def anonymize_(self, dicom_dir: str, tag_struct: str, send_params: dict, pv_id: int):
-
-        pl_id = self.__get_plugin_id({"name": "pl-dsdircopy", "version": "1.0.2"})
-        pl_sub_id = self.__get_plugin_id({"name": "pl-pfdicom_tagsub", "version": "3.3.4"})
-        pl_dcm_id = self.__get_plugin_id({"name": "pl-orthanc_push", "version": "1.2.7"})
-
-        # 1) Run dircopy
-        # empty directory check
-        if len(dicom_dir) == 0:
-            LOG(f"No directory found in CUBE containing files for search : {tag_struct}")
-            return
-        pv_in_id = self.__create_feed(pl_id, {"previous_id": pv_id, 'dir': dicom_dir})
-
-        # 2) Run anonymization
-        data = {"previous_id": pv_in_id, "tagStruct": tag_struct, 'fileFilter': '.dcm'}
-        tag_sub_id = self.__create_feed(pl_sub_id, data)
-
-        # 3) Push results
-        dir_send_data = {
-            "previous_id": tag_sub_id,
-            'inputFileFilter': "**/*dcm",
-            "orthancUrl": send_params["url"],
-            "username": send_params["username"],
-            "password": send_params["password"],
-            "pushToRemote": send_params["aec"]
-        }
-        pl_inst_id = self.__create_feed(pl_dcm_id, dir_send_data)
-
     def __create_feed(self, plugin_id: str,params: dict):
         response = self.cl.create_plugin_instance(plugin_id, params)
         return response['id']
