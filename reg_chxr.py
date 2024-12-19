e9a254b6658962ff806708153d6cf727ace39d02
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2024-12-19 18:07:40 -0500

    version bump

--- reg_chxr.py
+++ reg_chxr.py
@@ -25,7 +25,7 @@ logger_format = (
 logger.remove()
 logger.add(sys.stderr, format=logger_format)
 
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 
 DISPLAY_TITLE = r"""
        _                               _               
