0ae8061f274256ce42e88a93a6ea062dc49a6cf9
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2025-01-15 09:13:18 -0500

    version bump to push to store

--- reg_chxr.py
+++ reg_chxr.py
@@ -25,7 +25,7 @@ logger_format = (
 logger.remove()
 logger.add(sys.stderr, format=logger_format)
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 DISPLAY_TITLE = r"""
        _                               _               
